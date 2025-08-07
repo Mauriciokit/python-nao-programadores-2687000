939dfca31b684ff26619b310273d48d93f339f44
Mauricio Kit <mauriciokit@gmail.com>
2025-08-07 13:27:10 +0000

    Resolução de exercícios (Variáveis)

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,10 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = 'Mauricio'
 # Declare variáveis de cada tipo numérico
-
-
+numero_primo = 7
+salario = 12700.00
 # Declare uma variável do tipo booleano
+verdadeiro = True
+falso = False
