6aa668dec37fb0797fd9a44efc9ef84b155705d2
Mauricio Kit <mauriciokit@gmail.com>
2025-08-07 13:58:55 +0000

    Exercício - Numerico

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,15 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 # Realize uma operação entre dados do tipo string e inteiro
-
+texto = 'Mauricio'
+print(texto*3)
 
 # Realize uma operação entre dados do tipo inteiro e float
+print(altura+idade_numerica)
+x = (altura+idade_numerica)
+print(int(x))
+print(round(x))
