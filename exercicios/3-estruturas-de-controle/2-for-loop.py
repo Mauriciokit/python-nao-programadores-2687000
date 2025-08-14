c289aa3f700c0bf9886caba7ebaba6c5ee3a4151
Mauricio Kit <mauriciokit@gmail.com>
2025-08-14 15:13:00 +0000

    Exercícios - For Loop

--- exercicios/3-estruturas-de-controle/2-for-loop.py
+++ exercicios/3-estruturas-de-controle/2-for-loop.py
@@ -1,9 +1,14 @@
 # Crie uma lista
-
+cursos = ['SQL para iniciantes','EOS na prática','OKR e seus difereciais','Python para iniciantes','Estrutura de dados']
 # Crie um for loop para imprimir cada elemento dessa lista
-
+for cursor in cursos:
+  print(cursor)
 
 # Crie um objeto iterável utilizando a função range()
-
+ranger = [range(5)]
 
 # Use esse objeto iterável para criar um for loop que imprima na tela a frase "Estou aprendendo uma linguagem de programação."
+for ranger in range(5):
+  print(f"{ranger} - Estou aprendendo uma linguagem de programação.")
+else:
+  print("fim do loop")
